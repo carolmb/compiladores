f24f35148883b098fa174efd2ab6dc5ce18c56a8
Patricia Cruz <patipc@hotmail.com>
2018-04-09 11:08:10 +0000

    compiler

--- compiler.c
+++ compiler.c
@@ -5,8 +5,6 @@
 
 int main( int argc, char **argv ) {
     
-    std::cout << "NOT: " << NOT << " PROC: " << PROC << std::endl; 
-
     openFile(argc, argv);
     
     ++argv, --argc;  /* skip over program name */
