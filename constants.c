17bb7968a37ea16963bc72287f743f92b57d29f8
Patricia Cruz <patipc@hotmail.com>
2018-04-25 11:16:08 +0000

    Yacc init

--- constants.c
+++ constants.c
@@ -1,6 +1,6 @@
 #ifndef _CONSTANTS_
 #define _CONSTANTS_
-
+#include <stdbool.h>
 
 typedef enum {
 	NOT = '!',  
@@ -158,23 +158,23 @@ typedef enum {
 	ATOMICLISTAUX_
 } NONTERMINALS;
 
-bool is_terminal(int value){
-	if(value == 0) 
-		return true;
+// bool is_terminal(int value){
+// 	if(value == 0) 
+// 		return true;
 	
-	if(value >= NOT && value <= ERROR)
-		return true;
-	return false;
-}
+// 	if(value >= NOT && value <= ERROR)
+// 		return true;
+// 	return false;
+// }
 
-bool is_non_terminal(int value){
-	if(value >= PROGRAM_ && value <= ATOMICLISTAUX_)
-		return true;
-	return false;
-}
+// bool is_non_terminal(int value){
+// 	if(value >= PROGRAM_ && value <= ATOMICLISTAUX_)
+// 		return true;
+// 	return false;
+// }
 
-int idx(int value){
-	return value - NOT;
-}
+// int idx(int value){
+// 	return value - NOT;
+// }
 
 #endif
